ba98423c5e9a5e7e3f58670e4cb8c1faaa412e80
JolyNote <romdek174@gmail.com>
2022-04-16 18:48:50 +0500

    Version 1.0.1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,69 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('my-name', function () {
+    return ('Русанов Роман Витальевич');
+});
+
+Route::get('my-friend', function () {
+    return ('Кушнир Дмитрий Андреевич');
+});
+
+Route::get('get-friend/name', function ($nameFriend = 'Пивоваров Вадим Павлович') {
+    return $nameFriend;
+});
+
+Route::get('my-city/{city}', function ($city) {
+    return $city;
+});
+
+Route::get('level/{lvl}', function ($lvl) {
+    if(($lvl>=0)&&($lvl<=25)){
+        return 'новичок';}
+    if(($lvl>=26)&&($lvl<=50)){
+        return 'специалист';}
+    if(($lvl>=51)&&($lvl<=75)){
+        return 'босс';}
+    if(($lvl>=76)&&($lvl<=98)){
+        return 'старик';}
+    if($lvl==99){
+        return 'король';}
+    else return 'ошибка';
+});
+
+Route::get('working/{name}/{date?}', function ($name, $date) {
+    return ('Название проекта: '.$name.' Дата выполнения проекта:'.$date) ;
+});
+
+Route::get('working/{name}/{date?}', function ($name, $date) {
+    return ('Название проекта: '.$name.' Дата выполнения проекта:'.$date) ;
+});
+
+Route::get('/power/power', function () {
+    return '<a href="/test">узнать данный путь</a>';
+})->name('power');
+Route::get('/test', function(){
+    return route('power');
+});
+
+Route::prefix('admin')->group(function (){
+    Route::get('/login', function (){
+        return 'админ логин';
+    });
+    Route::get('/logout', function (){
+        return 'админ логаут';
+    });
+    Route::get('/info', function (){
+        return 'админ инфо';
+    });
+    Route::get('/color', function (){
+        return 'админ цвет';
+    });
+});
+
+Route::redirect('/admin/web', '/admin/color');
+
+Route::get('/color/{hex}', function ($hex){
+    return $hex;
+}) -> where('hex', '[0-9a-fA-F]{6}+');
