6705b857c38886b82fe91866b0a1d77994d8effb
is42-rusanovrv <romdek174@gmail.com>
2022-04-28 17:10:37 +0500

    version 1.0.4

--- resources/views/div.blade.php
+++ resources/views/div.blade.php
@@ -6,22 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-        div{
-            background-color: darkcyan;
-            border-radius: 20px;
-            padding: 10px;
-            color: white;
-            font-size: 15pt;
-            width: 2.5em;
-            margin: 10px;
-            text-align: center;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 @foreach($var_div as $div)
-    <div>{{$div}}</div>
+    <div class="container">
+        <div class="row g-4 text-white">
+            <div class="col-6">
+                <div class="p-3 m-2 border text-center bg-primary">{{$div}}</div>
+            </div>
+            <div class="col-6">
+                <div class="p-3 m-2 border text-center bg-primary">{{$div}}</div>
+            </div>
+        </div>
+    </div>
 @endforeach
 </body>
 </html>
+
